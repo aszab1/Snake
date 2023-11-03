93bb4eddba1b6c43707d08dc2202b93c6d39e9ab
aszab1 <adry_szabo@yahoo.co.uk>
2023-11-03 15:26:23 +0000

    starter HTML

---
+++ scripts /app.js
@@ -0,0 +1,60 @@
+// * Elements
+// Start button DOM element: startButton (.start class)
+// Grid (const grid = document.queryselector(.grid))
+// Cells (const cells [])
+// Initial speed of snake 
+// direction
+// potentially - score display (.score class)
+// potentially - high-score (class)
+
+// * Variables
+// snake  / initial position
+// food  /intial position 
+// gameInterval
+// Is game active? - boolean (let gameActive - false)
+// potentially Start score at 0
+// 
+
+// * Grid 
+// Making the grid in JS 
+// width (this will be both the width and the height const width = )
+// cellcount - represents the number of cells in the grid (const cellCount = width * width)
+// function to create all the grid cells and append them to the existing grid 
+// set width and height of the div cells
+
+// Function to update the grid with the starting snake and food position
+// add snake (poss .forEach)
+// food (poss const = foodIdx)
+
+// * Executions
+// Click start button to start the game;  
+// check if gamesactive - reset if not,  
+// set game interval to inital speed 
+// function to move the snake - const head  /  set direction up, down, left, right
+// if head is touching food - push food to head poss if/else / if checkCollision()  endGame /reset
+
+// function to generate random food positions (with Math.floor(Math.random))
+// function , let foodposition = ,
+
+// function to check for collision (wall and self)
+// function checkCollision ()
+
+// function to end the game 
+// function endGame () clearInterval=gameinterval
+// alert Game over (and potentially score)
+
+// fuction to reset game 
+// snake, food position, direction, displayGrid ()
+//  potentially score / update high-score 
+
+
+
+
+//  * EventListeners
+// Start button: click --> startGame function
+// keyboard input - (document.addEvenlistener)
+
+
+
+// * Page Load 
+//createGrid()
