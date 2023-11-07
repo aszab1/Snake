6ff0f2d3297bc60a01a31d8496a57e1bf4e6c205
aszab1 <adry_szabo@yahoo.co.uk>
2023-11-07 15:41:55 +0000

    added CSS

--- scripts /app.js
+++ scripts /app.js
@@ -34,6 +34,7 @@ const width = 20
 // cellcount - represents the number of cells in the grid (const cellCount = width * width)
 const cellCount = width * width
 // function to create all the grid cells and append them to the existing grid 
+// uses a loop to create cellCount number of cells 
 function createGrid(){
   for (let i = 0; i < cellCount; i++) {
     const cell = document.createElement('div')
@@ -52,7 +53,6 @@ function createGrid(){
 createGrid()
 
 
-
 // Function to update the grid with the starting snake and food position
 // add snake 
 function addSnake(position){
@@ -62,66 +62,90 @@ function addSnake(position){
 function addFood(position){
   cells[position].classList.add('food')
 }
+
+// * Execution 
+
 //function to move snake
+//first check if gameActive - if false, return from the function
 function moveSnake() { 
   if (!gameActive) return
 
-  //snake collision 
+  // calculates the the head's next position based on its currentSnakesPos and the direction its moving
   const head = currentSnakePos[currentSnakePos.length - 1] + direction 
-
+  //snake collision 
+  // checks if the head is out of the grid boundaries ( >= cellCount)
   if (head < 0 || head >= cellCount || 
   //right border collision
   (direction === 1 && head % width === 0) || 
   //left border collision
   (direction === -1 && head % width === width - 1) || 
+  // collides with own body
   cells[head].classList.contains('snake')) {
     endGame() 
     return 
   }
+  //add new head position to the currentSnakePos array, representing the snakes body
   currentSnakePos.push(head)
   
+  //checks if cell and  head position contains food class. if it does, snake ate food
   if (cells[head].classList.contains('food')) { 
+    // update score by 1
     score++ 
+    // update scoreSpan
     scoreSpan.textContent = score 
+    // remove food class from cell
     cells[head].classList.remove('food') 
-    addFood(getrandomFoodPosition())
-    //increase speed by 5ms after each food eaten starting at 200
+    //calls addFood function to add new food using the getrandomFoodposition
+    addFood(getRandomFoodPosition())
+    //clear existing interval time
     clearInterval(gameInterval)
+    // increase speed by 5ms after each food eaten starting at 200
     gameInterval = setInterval(moveSnake, 200 - (score * 5))
   } else {
+    // remove tail position from currentSnakePos array and assign it to tail
     const tail = currentSnakePos.shift()
+    // remove the snake class from the cell at the previous tail pos
     cells[tail].classList.remove('snake')
   }
+  // add the snake class to the new head position 
   cells[head].classList.add('snake')
 }
-console.log(moveSnake)
 
-function getrandomFoodPosition() {
+
+//function to generate a random food position on the grid
+
+function getRandomFoodPosition() {
   let foodPosition 
   do {
     foodPosition = Math.floor(Math.random() * cellCount)
   } while (
+    // making sure the food position doesn't overlap with the snake 
     currentSnakePos.includes(foodPosition) ||
     cells[foodPosition].classList.contains('food')) 
 
   return foodPosition
 }
-
+//function endGame 
+// stopping the game interval and displaying final score
 function endGame() {
   gameActive = false
   clearInterval(gameInterval)
   alert(`Game over! Final score: ${score}`)
 
+  // resetting the snake's inital position, direction, score 
   currentSnakePos = startSnakePos
   direction = 1
   score = 0
   scoreSpan.textContent = 0
 
+  //removing snake and food classes from all cells 
   cells.forEach((cell) => cell.classList.remove('snake', 'food'))
   addSnake(currentSnakePos)
   addFood(startFoodPos)
 }
 
+//function for direction 
+//handles the direction change of the snake based on the key pressed 
 function changeDirection(evt) {
   const key = evt.code
   if (key === 'ArrowUp' && direction !== width) {
@@ -135,6 +159,7 @@ function changeDirection(evt) {
   }
 }
 
+// * Event Listeners 
 
 startBtn.addEventListener('click', () => {
   if (!gameActive) {
